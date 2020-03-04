55f4a6c9e4768220b30011a9bf60d08d446ab6b0
ryan <ryanwaszak@gmail.com>
2020-03-03 23:40:02 -0600

    update /show_name to tell user if name starts with first letter of alphabet

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -1,6 +1,11 @@
 class Api::ParamsController < ApplicationController
   def show_name
-    @name = params["name"].upcase
+    @result = params["name"].upcase
+    if @result[0].downcase == "a"
+      @result = "Hey, your name starts with the first letter of the alphabet, #{@result}!"
+    else
+      @result
+    end
     render "show_name.json.jb"
   end
 end
